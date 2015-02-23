89ec24cb55000f01b5c09ecf05732369310cc1a5
Christian Holl <CyborgX1@gmail.com>
2015-02-23 16:50:12 +0100

    add comments add extern C

--- sersyncproto.c
+++ sersyncproto.c
@@ -9,9 +9,9 @@
 uint8_t sersyncproto_send(sersyncproto_data_t* data, uint8_t cmd, uint8_t* payload, void (*sendbyte)(uint8_t) )
 {
 	uint8_t found=0;
+	//Check for the command
 	for (uint8_t c = 0; c < data->_cmd_cnt; ++c)
 	{
-
 		if(data->_cmd_array[c]==cmd)
 		{
 			found=1;
@@ -19,29 +19,36 @@ uint8_t sersyncproto_send(sersyncproto_data_t* data, uint8_t cmd, uint8_t* paylo
 			break;
 		}
 	}
+	//if command found
 	if(found)
 	{
+		//send header
 		for (int hd = 0; hd < data->_header_len; ++hd)
 		{
 			sendbyte(data->_header[hd]);
 		}
 
+		//start checksum with cmd
 		uint8_t chksum=cmd;
+
+		//send cmd byte
 		sendbyte(cmd);
 
+		//send payloads if there is any payload for that command
 		for (int pb = 0; pb < data->_payload_lengths[cmd]; ++pb)
 		{
 			chksum^=payload[pb];
 			sendbyte(payload[pb]);
 		}
+
+		//send checksum
 		sendbyte(chksum);
 	}
-	else
+	else //Command not found
 	{
 		return 0;
 	}
-
-	return 1;
+	return 1;//Ok
 }
 
 uint8_t sersyncproto_rec(sersyncproto_data_t* data, uint8_t cur_byte)
@@ -54,39 +61,45 @@ uint8_t sersyncproto_rec(sersyncproto_data_t* data, uint8_t cur_byte)
 
 	switch(data->_state)
 	{
-
+		//Waiting for the header to pass
 		case SERSYNCPROTO_STATE_HEADER:
 
+			//If current data is the same like it should be in the header
 			if(cur_byte==data->_header[data->_length])
 			{
-
+				//Increase current length (header)
 				data->_length++;
-				if(data->_length == data->_header_len)
+				if(data->_length == data->_header_len)//Header length reached?
 				{
-					data->_state=SERSYNCPROTO_STATE_CMD;
-					data->_length=0;
-					data->_chksum=0;
+					data->_state=SERSYNCPROTO_STATE_CMD; //Go to command identification
+					data->_length=0; //Length reset
+					data->_chksum=0; //checksum reset
 				}
 			}
 			else
 			{
+				//Restart header parser if not the header
 				data->_state=SERSYNCPROTO_STATE_HEADER;
 				data->_length=0;
 			}
 			break;
 
+			//Get the command number
 		case SERSYNCPROTO_STATE_CMD:
 			for (int c = 0; c < data->_cmd_cnt; ++c)
 			{
+				//Check if we have that command
 				if(data->_cmd_array[c]==cur_byte)
 				{
-
+					//Does it have a payload?
 					if(data->_payload_lengths[c]>0)
 					{
+						//Goto payload state
 						data->_state=SERSYNCPROTO_STATE_PAYLOAD;
 					}
 					else
 					{
+						//Go directly to checksum
 						data->_state=SERSYNCPROTO_STATE_CHKSUM;
 					}
 					data->cmd=c;
@@ -96,25 +109,31 @@ uint8_t sersyncproto_rec(sersyncproto_data_t* data, uint8_t cur_byte)
 
 			if(data->_state==SERSYNCPROTO_STATE_CMD) //Command not found?
 			{
+				//Ignore it and wait for next header
 				data->_state=SERSYNCPROTO_STATE_HEADER;
 				data->_length=0;
 			}
 			break;
 
+			//Store payload
 		case SERSYNCPROTO_STATE_PAYLOAD:
 			data->payload[data->_length]=cur_byte;
 			data->_length++;
 
+			//Payload received?
 			if(data->_length==data->_payload_lengths[data->cmd])
 			{
+				//Goto checksum
 				data->_state=SERSYNCPROTO_STATE_CHKSUM;
 			}
 
 			break;
 
+			//Checksum
 		case SERSYNCPROTO_STATE_CHKSUM:
-			if(data->_chksum==cur_byte)
+			if(data->_chksum==cur_byte)//checksum equal to current byte?
 			{
+				//Reset for next header, return 1
 				data->_state=SERSYNCPROTO_STATE_HEADER;
 				data->_length=0;
 				return 1;
@@ -122,6 +141,7 @@ uint8_t sersyncproto_rec(sersyncproto_data_t* data, uint8_t cur_byte)
 			break;
 
 		default:
+			//Reset for next header
 			data->_state=SERSYNCPROTO_STATE_HEADER;
 			data->_length=0;
 			break;
--- sersyncproto.h
+++ sersyncproto.h
@@ -10,7 +10,11 @@
 
 #include <inttypes.h>
 
+#ifdef __cplusplus
+	extern "C"
+	{
 
+#endif
 
 
 typedef enum
@@ -25,28 +29,48 @@ typedef enum
 
 typedef struct
 {
-	uint8_t const _cmd_cnt;
-	uint8_t const * const _cmd_array;
-	uint8_t const * const _payload_lengths;
+	uint8_t const _cmd_cnt; //!< Amount of cmds available
+	uint8_t const * const _cmd_array; //!< Array of possible commands
+	uint8_t const * const _payload_lengths; //!< payload length of each command in cmd_array
 
-	uint8_t const _header_len;
-	uint8_t const * const _header;
+	uint8_t const _header_len; //!< Length of header
+	uint8_t const * const _header; //!< Header definition
 
-	sersyncproto_state_t _state;
-	uint8_t _length;
-	uint8_t cmd;
-	uint8_t _chksum;
+	sersyncproto_state_t _state; //!< Current state of the protocol state machine
+	uint8_t _length; //!< Variable store the current length of a step (not for user)
+	uint8_t cmd; //!< When the parser function returns true this contains the cmd
+	uint8_t _chksum; //!< Variable stores the checksum
 
-	uint8_t *payload;
+	uint8_t *payload; //!< Points to the space where the parser should store payload, minsize==max payload size
 }sersyncproto_data_t;
 
 #define INIT_SERSYNCPROTO_DATA(DATA, CMD_ARRAY, PAYLOAD_LEN_ARRAY, HEADER_ARRAY, PAYLOAD_BUFFER)\
-		static sersyncproto_data_t DATA={sizeof(CMD_ARRAY), CMD_ARRAY, PAYLOAD_LEN_ARRAY, sizeof(HEADER_ARRAY), HEADER_ARRAY, SERSYNCPROTO_STATE_HEADER,0,0,0, PAYLOAD_BUFFER};
+		static sersyncproto_data_t DATA={sizeof(CMD_ARRAY), CMD_ARRAY, PAYLOAD_LEN_ARRAY, sizeof(HEADER_ARRAY), HEADER_ARRAY, SERSYNCPROTO_STATE_HEADER,0,0,0, (uint8_t*)PAYLOAD_BUFFER};
 
 #define SERSYNCPROTO_GET_CUR_CMD(DATA)\
 		DATA._cmd_array[DATA.cmd]
 
-
+/**
+ * Receiving function
+ * needs to be called for each byte.
+ * @param cur_byte The current byte
+ * @param data The data struct containing the definitions
+ */
 uint8_t sersyncproto_rec(sersyncproto_data_t* data, uint8_t cur_byte);
+
+/**
+ * Sending function
+ * @param data The data struct containing the definitions
+ * @param cmd The command to be issued
+ * @param payload The payload of the current function
+ * @param sendbyte The USER_IMPLEMENTATION of a function which sends the byte over the serial interface
+ */
+
 uint8_t sersyncproto_send(sersyncproto_data_t* data, uint8_t cmd, uint8_t* payload, void (*sendbyte)(uint8_t) );
+
+
+#ifdef __cplusplus
+	}
+#endif
+
 #endif /* SERSYNCPROTO_H_ */
